19a71ee8087fb63449b98bcd5a9682057ae9b3f4
Donavan White <digi.cazter@gmail.com>
2012-11-09 09:29:34 -0700

    Fixed devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -59,5 +59,9 @@ Onyx::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
+  devise_scope :user do
+    get "sign_in", :to => "devise/sessions#new"
+  end
+
   root :to => "dashboard#index"
 end
